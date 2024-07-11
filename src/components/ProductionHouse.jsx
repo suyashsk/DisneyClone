ea3000196d750472a8a714d4f24e348e5878e76f
suyashsk <suyashkakade.sk03@gmail.com>
2024-07-12 01:18:49 +0530

    path changed

--- src/components/ProductionHouse.jsx
+++ src/components/ProductionHouse.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import disney from '../assets/Images/disneyone.png'
-import marvel from '../assets/Images/marvel.png'
-import nationalG from '../assets/Images/nationalG.png'
-import pixar from '../assets/Images/pixar.png'
-import starwar from '../assets/Images/starwar.png'
+import disney from './../assets/Images/disneyone.png'
+import marvel from './../assets/Images/marvel.png'
+import nationalG from './../assets/Images/nationalG.png'
+import pixar from './../assets/Images/pixar.png'
+import starwar from './../assets/Images/starwar.png'
  
-import starwarV from '../assets/Videos/star-wars.mp4'
-import disneyV from '../assets/Videos/disneyo.mp4'
-import marvelV from '../assets/Videos/marvel.mp4'
-import nationalGeographicV from '../assets/Videos/national-geographic.mp4'
-import pixarV from '../assets/Videos/pixar.mp4'
+import starwarV from './../assets/Videos/star-wars.mp4'
+import disneyV from './../assets/Videos/disneyo.mp4'
+import marvelV from './../assets/Videos/marvel.mp4'
+import nationalGeographicV from './../assets/Videos/national-geographic.mp4'
+import pixarV from './../assets/Videos/pixar.mp4'
 
 const ProductionHouse = () => {
     const productionHouseList=[
